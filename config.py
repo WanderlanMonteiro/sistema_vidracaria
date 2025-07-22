eef205e3c2ea0210fcf60eefbca759dad21c9f5e
WanderlanMonteiro <wmdshere@gmail.com>
2025-07-22 01:30:39 -0300

    Atualizei conexão com PostgreSQL e adicionei arquivos do sistema

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://usuario:senha@localhost:5432/meubanco')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://db_dados_hnsj_user:rglNUlPon4CWus77gWG6Fksp37Jic9mR@dpg-d1vdqrjuibrs739achm0-a.oregon-postgres.render.com/db_dados_hnsj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'sua_chave_super_secreta'
+
