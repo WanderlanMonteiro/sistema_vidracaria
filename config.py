42cc6cfade0b50d295997d578ab374f1aaa23067
WanderlanMonteiro <wmdshere@gmail.com>
2025-07-24 01:46:45 -0300

    atualizando app e config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Idss2010@')
+    SECRET_KEY = os.environ.get("SECRET_KEY", "Idss2010@")
     ENV = os.environ.get("FLASK_ENV", "production")
 
 class DevelopmentConfig(Config):
