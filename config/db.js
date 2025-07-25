f090be82c7eef653721d88adf609c35b99f3e78d
WanderlanMonteiro <wmdshere@gmail.com>
2025-07-24 23:55:19 -0300

    ajuste do arquivo config/db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  'postgres', // nome do banco de dados (ou 'wmvidrosemarmore' se quiser usar o outro banco)
-  'wmdshere', // usuário completo para conectar no Azure
-  'idss2010@', // senha fornecida
-  {
-    host: 'vidracaria-marmore.postgres.database.azure.com',
-    dialect: 'postgres',
-    port: 5432,
-    dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false } // Azure exige SSL
-    },
-    logging: false // desabilita logs SQL; mude para true se quiser ver comandos executados
+const sequelize = new Sequelize('postgres', 'wmdshere', 'Idss2010@', {
+  host: 'vidracaria-marmore.postgres.database.azure.com',
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 module.exports = sequelize;
