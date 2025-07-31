f55863dd46fd27dcd4e436f9c86bcf1f80385d07
WanderlanMonteiro <wmdshere@gmail.com>
2025-07-31 01:15:14 -0300

    atualizando rotas v 1.0

--- models/Ususario.js
+++ models/Ususario.js
@@ -2,14 +2,26 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Usuario = sequelize.define('Usuario', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nome: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  senha: { type: DataTypes.STRING, allowNull: false },
-  tipo: { type: DataTypes.STRING } // admin, vendedor, etc.
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 }, {
   tableName: 'usuarios',
-  timestamps: true
+  timestamps: false
 });
 
 module.exports = Usuario;
