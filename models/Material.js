29d59bd163007bcc4e3c43b416ed9dc6a65048d6
WanderlanMonteiro <wmdshere@gmail.com>
2025-07-31 00:55:55 -0300

    atualizando rotas v 1.0

--- models/Material.js
+++ models/Material.js
@@ -1,14 +1,47 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
+// Importação dos outros models para criar os relacionamentos
+const Corte = require('./Corte');
+
+// Definição do model Material
 const Material = sequelize.define('Material', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nome: { type: DataTypes.STRING, allowNull: false },
-  tipo: { type: DataTypes.STRING }, // vidro, perfil, acessório, etc.
-  descricao: { type: DataTypes.TEXT }
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tipo: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  preco: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 0
+  }
 }, {
   tableName: 'materiais',
-  timestamps: true
+  timestamps: false
+});
+
+// RELACIONAMENTOS
+// Um Material pode estar em vários Cortes
+Material.hasMany(Corte, {
+  foreignKey: 'material_id',
+  as: 'cortes'
+});
+Corte.belongsTo(Material, {
+  foreignKey: 'material_id',
+  as: 'material'
 });
 
 module.exports = Material;
